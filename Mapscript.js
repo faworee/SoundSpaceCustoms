0bffee9dbcfb29a4228f8a48b84b0ee6f8cdcac9
faworee <121886277+faworee@users.noreply.github.com>
2025-12-18 02:16:56 +0100

    Update Mapscript.js

--- Mapscript.js
+++ Mapscript.js
@@ -2530,7 +2530,7 @@ async function loadMapData() {
 async function loadMapCopies() {
     try {
         const timestamp = Date.now();
-        const response = await fetch('https://faworee.com/soundspacecustoms/get-mapcopies.php?v=' + timestamp);
+        const response = await fetch('https://faworee.com/soundspacecustoms/MapCopies.json');
         if (!response.ok) throw new Error('Failed to load map copies data');
         
         mapCopiesData = await response.json();
